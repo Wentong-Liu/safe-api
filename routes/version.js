62d4a0f0cc18ada4672011289e03446a2e427e4c
Wentong Liu <wentongl@student.unimelb.edu.au>
2018-07-28 03:20:33 +1000

    Remove the now ignored .gitignore

--- routes/version.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {apiVersion} = require('../config');
-
-router.use(`/v${apiVersion}`, require('./requests'));
-
-module.exports = router;
