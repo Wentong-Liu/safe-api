557acba7817bd602bf1fd346394628b1b78686b0
Wentong Liu <wentongl@student.unimelb.edu.au>
2018-08-08 22:43:19 +1000

    add pm2 config

--- utils/sign.js
+++ utils/sign.js
@@ -4,5 +4,5 @@ const crypto = require('crypto');
 
 module.exports = ({url, payload, token, timestamp, nonce, secret}) => {
     const plain = url + JSON.stringify(payload) + (token === undefined ? '' : token) + timestamp + nonce;
-    return crypto.createHash('sha256', secret === undefined ? predefinedSecret : secret).update(plain).digest('hex');
+    return crypto.createHmac('sha256', secret === undefined ? predefinedSecret : secret).update(plain).digest('hex');
 };
